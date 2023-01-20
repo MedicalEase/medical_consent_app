349291513f4bd3ab423612a18f3e6d2e66470022
jharley <ratter.char0h@icloud.com>
2023-01-19 21:12:34 -0400

    add simple video paging

---
+++ consent_app/consent_app/android/app/src/main/kotlin/medicalEase/consent_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package medicalEase.consent_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ consent_app/consent_app/lib/src/sample_feature/sample_item_details_view.dart
@@ -0,0 +1,65 @@
+import 'package:consent_app/src/sample_feature/sample_item.dart';
+import 'package:consent_app/src/sample_feature/sample_item_list_view.dart';
+import 'package:flutter/material.dart';
+
+class SampleItemDetailsView extends StatelessWidget {
+  // In the constructor, require a videoId.
+  const SampleItemDetailsView({super.key, required this.videoId});
+
+  // Declare a field that holds the videoId.
+  final int videoId;
+  static const routeName = '/detail';
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the videoId to create the UI.
+    SampleItem item = const SampleItemListView().items[videoId];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hed'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(videoId.toString()),
+            Text(item.id.toString()),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              child: const Text('Next'),
+              onPressed: () {
+                // When the user taps the button, navigate to the next video.
+                // Navigate to the next video.
+                Navigator.pushNamed(
+                  context,
+                  routeName,
+                  arguments: videoId + 1,
+                );
+
+                // When the user taps the button, navigate back to the first
+                // screen by popping the current route off the stack.
+                Navigator.restorablePushNamed(
+                  context,
+                  SampleItemDetailsView.routeName,
+                  arguments: videoId + 1,
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('home'),
+              onPressed: () {
+                // When the user taps the button, navigate back to the first
+                // screen by popping the current route off the stack.
+                Navigator.restorablePushNamed(
+                  context,
+                  SampleItemListView.routeName,
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
