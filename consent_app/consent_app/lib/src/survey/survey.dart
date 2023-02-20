7544d7c551730542077d86711093411bab23ba9a
jharley <ratter.char0h@icloud.com>
2023-02-20 11:12:11 -0400

    add survey & questions

--- consent_app/consent_app/lib/src/survey/survey.dart
+++ consent_app/consent_app/lib/src/survey/survey.dart
@@ -50,272 +50,215 @@ class SurveyApp extends StatefulWidget {
 class _SurveyAppState extends State<SurveyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          color: Colors.white,
-          child: Align(
-            alignment: Alignment.center,
-            child: FutureBuilder<Task>(
-              future: getJsonTask(),
-              builder: (context, snapshot) {
-                // todo : check all these conditions are correct
-                if (snapshot.connectionState == ConnectionState.done &&
-                    snapshot.hasData &&
-                    snapshot.data != null) {
-                  final task = snapshot.data!;
-                  return SurveyKit(
-                    onResult: (SurveyResult result) {
-                      print(result.finishReason);
-                      // Navigator.pushNamed(context, '/');
-                      Navigator.restorablePushNamed(
-                        context,
-                        FinalThankYou.routeName,
-                      );
-                    },
-                    task: task,
-                    showProgress: true,
-                    localizations: {
-                      'cancel': 'Cancel'.i18n,
-                      'next': 'Next'.i18n,
-                    },
-                    themeData: Theme.of(context).copyWith(
-                      primaryColor: Colors.cyan,
-                      appBarTheme: const AppBarTheme(
-                        color: Colors.white,
-                        iconTheme: IconThemeData(
-                          color: Colors.cyan,
-                        ),
-                        titleTextStyle: TextStyle(
-                          color: Colors.cyan,
-                        ),
-                      ),
-                      iconTheme: const IconThemeData(
-                        color: Colors.cyan,
-                      ),
-                      textSelectionTheme: TextSelectionThemeData(
-                        cursorColor: Colors.cyan,
-                        selectionColor: Colors.cyan,
-                        selectionHandleColor: Colors.cyan,
-                      ),
-                      cupertinoOverrideTheme: CupertinoThemeData(
-                        primaryColor: Colors.cyan,
-                      ),
-                      outlinedButtonTheme: OutlinedButtonThemeData(
-                        style: ButtonStyle(
-                          minimumSize: MaterialStateProperty.all(
-                            Size(150.0, 60.0),
-                          ),
-                          side: MaterialStateProperty.resolveWith(
-                            (Set<MaterialState> state) {
-                              if (state.contains(MaterialState.disabled)) {
-                                return BorderSide(
-                                  color: Colors.grey,
-                                );
-                              }
-                              return BorderSide(
-                                color: Colors.cyan,
-                              );
-                            },
-                          ),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                          ),
-                          textStyle: MaterialStateProperty.resolveWith(
-                            (Set<MaterialState> state) {
-                              if (state.contains(MaterialState.disabled)) {
-                                return Theme.of(context)
-                                    .textTheme
-                                    .labelLarge
-                                    ?.copyWith(
-                                      color: Colors.grey,
-                                    );
-                              }
-                              return Theme.of(context)
-                                  .textTheme
-                                  .labelLarge
-                                  ?.copyWith(
-                                    color: Colors.cyan,
-                                  );
-                            },
-                          ),
-                        ),
-                      ),
-                      textButtonTheme: TextButtonThemeData(
-                        style: ButtonStyle(
-                          textStyle: MaterialStateProperty.all(
-                            Theme.of(context).textTheme.labelLarge?.copyWith(
-                                  color: Colors.cyan,
-                                ),
-                          ),
-                        ),
-                      ),
-                      textTheme: const TextTheme(
-                        displayMedium: TextStyle(
-                          fontSize: 28.0,
-                          color: Colors.black,
-                        ),
-                        headlineSmall: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.black,
-                        ),
-                        bodyMedium: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.black,
-                        ),
-                        titleMedium: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.black,
-                        ),
-                      ),
-                      inputDecorationTheme: const InputDecorationTheme(
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                      colorScheme: ColorScheme.fromSwatch(
-                        primarySwatch: Colors.cyan,
-                      )
-                          .copyWith(
-                            onPrimary: Colors.white,
-                          )
-                          .copyWith(background: Colors.white),
-                    ),
-                    surveyProgressbarConfiguration: SurveyProgressConfiguration(
-                      backgroundColor: Colors.white,
-                    ),
+    return Container(
+      color: Colors.white,
+      child: Align(
+        alignment: Alignment.center,
+        child: FutureBuilder<Task>(
+          future: getQuizTask(),
+          builder: (context, snapshot) {
+            // todo : check all these conditions are correct
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData &&
+                snapshot.data != null) {
+              final task = snapshot.data!;
+              return SurveyKit(
+                onResult: (SurveyResult result) {
+                  print(result.finishReason);
+                  // Navigator.pushNamed(context, '/');
+                  Navigator.restorablePushNamed(
+                    context,
+                    FinalThankYou.routeName,
                   );
-                }
-                return CircularProgressIndicator.adaptive();
-              },
-            ),
-          ),
+                },
+                task: task,
+                showProgress: true,
+                localizations: {
+                  'cancel': 'Cancel'.i18n,
+                  'next': 'Next'.i18n,
+                },
+                themeData: Theme.of(context).copyWith(
+                  colorScheme: ColorScheme.light(
+                      primary: Color(0xFF005EB8),
+                      secondary: Color(0xFF005EB8),
+                      background: Colors.green
+                  ),
+                  appBarTheme: const AppBarTheme(
+                    color: Color(0xFFF0F4F5),
+                    iconTheme: IconThemeData(
+                      color: Color(0xFF005EB8),
+                    ),
+                  ),
+                  cupertinoOverrideTheme: CupertinoThemeData(
+                  ),
+                  outlinedButtonTheme: OutlinedButtonThemeData(
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(
+                        Size(150.0, 60.0),
+                      ),
+                      side: MaterialStateProperty.resolveWith(
+                            (Set<MaterialState> state) {
+                          if (state.contains(MaterialState.disabled)) {
+                            return BorderSide(
+                              color: Colors.grey,
+                            );
+                          }
+                          return BorderSide(
+                            color: Colors.cyan,
+                          );
+                        },
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  textButtonTheme: TextButtonThemeData(
+                    style: ButtonStyle(),
+                  ),
+                  textTheme: const TextTheme(
+                      displayMedium: TextStyle(
+                        fontSize: 28.0,
+                        color: Colors.black,
+                      )),
+                  inputDecorationTheme: const InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+
+                surveyProgressbarConfiguration: SurveyProgressConfiguration(
+                  backgroundColor: Color(0xFFFFEB3B),
+                  progressbarColor: Color(0xFFFFEB3B),
+                  height: 18,
+                ),
+              );
+            }
+            return CircularProgressIndicator.adaptive();
+          },
         ),
       ),
     );
   }
 
-  Future<Task> getSampleTask() {
+  Future<Task> getQuizTask() {
     var task = NavigableTask(
       id: TaskIdentifier(),
       steps: [
         InstructionStep(
-          title: 'Welcome to the\nQuickBird Studios\nHealth Survey',
-          text: 'Get ready for a bunch of super random questions!',
-          buttonText: 'Let\'s go!',
+          title: 'Thank you for taking this survey',
+          text: 'Please answer the following ten short questions',
+          buttonText: 'Start',
         ),
         QuestionStep(
-          title: 'How old are you?',
-          answerFormat: IntegerAnswerFormat(
-            defaultValue: 25,
-            hint: 'Please enter your age',
-          ),
-          isOptional: true,
-        ),
-        QuestionStep(
-          title: 'Medication?',
-          text: 'Are you using any medication',
-          answerFormat: BooleanAnswerFormat(
-            positiveAnswer: 'Yes',
-            negativeAnswer: 'No',
-            result: BooleanResult.POSITIVE,
-          ),
-        ),
-        QuestionStep(
-          title: 'Tell us about you',
-          text:
-              'Tell us about yourself and why you want to improve your health.',
-          answerFormat: TextAnswerFormat(
-            maxLines: 5,
-            validationRegEx: "^(?!\s*\$).+",
-          ),
-        ),
-        QuestionStep(
-          title: 'Select your body type',
-          answerFormat: ScaleAnswerFormat(
-            step: 1,
-            minimumValue: 1,
-            maximumValue: 5,
-            defaultValue: 3,
-            minimumValueDescription: '1',
-            maximumValueDescription: '5',
-          ),
-        ),
-        QuestionStep(
-          title: 'Known allergies',
-          text: 'Do you have any allergies that we should be aware of?',
-          isOptional: false,
-          answerFormat: MultipleChoiceAnswerFormat(
-            textChoices: [
-              TextChoice(text: 'Penicillin', value: 'Penicillin'),
-              TextChoice(text: 'Latex', value: 'Latex'),
-              TextChoice(text: 'Pet', value: 'Pet'),
-              TextChoice(text: 'Pollen', value: 'Pollen'),
-            ],
-          ),
-        ),
-        QuestionStep(
-          title: 'Done?',
-          text: 'We are done, do you mind to tell us more about yourself?',
-          isOptional: true,
+          title: 'Was it made clear to you WHAT procedure you were having?'
+              .i18n,
           answerFormat: SingleChoiceAnswerFormat(
             textChoices: [
               TextChoice(text: 'Yes', value: 'Yes'),
               TextChoice(text: 'No', value: 'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
             ],
-            defaultSelection: TextChoice(text: 'No', value: 'No'),
+          ),
+
+          isOptional: true,
+        ),
+        QuestionStep(
+          title: 'Was it made clear to you WHY you were having the procedure?',
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
           ),
         ),
         QuestionStep(
-          title: 'When did you wake up?',
-          answerFormat: const TimeAnswerFormat(
-            defaultValue: TimeOfDay(
-              hour: 12,
-              minute: 0,
-            ),
+          title: 'Were the risks of the procedure explained to you?',
+          text: '',
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
           ),
         ),
         QuestionStep(
-          title: 'When was your last holiday?',
-          answerFormat: DateAnswerFormat(
-            minDate: DateTime.utc(1970),
-            defaultDate: DateTime.now(),
-            maxDate: DateTime.now(),
+          title: 'Were the options of anaesthetic spray and sedation made clear to you?',
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
+          ),
+        ),
+        QuestionStep(
+          title: 'Were the RISKS of sedation made clear to you?',
+          text: '',
+          isOptional: false,
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
+          ),
+        ),
+        QuestionStep(
+          title: 'Were you told you would need an escort if you chose to have sedation?',
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
+          ),
+        ),
+        QuestionStep(
+          title: 'Were you told that you would need to have someone '
+              'with you for 24 hours if you chose to have sedation?',
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
+          ),
+        ),
+        QuestionStep(
+          title: 'Did the doctor/nurse ask whether you had any questions?',
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
+          ),
+        ),
+        QuestionStep(
+          title: 'Were your questions answered to your satisfaction?',
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value:'No'),
+              TextChoice(text: 'Not Sure', value: 'Not Sure'),
+            ],
           ),
         ),
         CompletionStep(
           stepIdentifier: StepIdentifier(id: '321'),
-          text: 'Thanks for taking the survey, we will contact you soon!',
+          text: 'Thanks for taking the survey, your feedback is valuable to '
+              'us',
           title: 'Done!',
-          buttonText: 'Submit survey',
+          buttonText: 'Complete',
         ),
       ],
     );
-    task.addNavigationRule(
-      forTriggerStepIdentifier: task.steps[6].stepIdentifier,
-      navigationRule: ConditionalNavigationRule(
-        resultToStepIdentifierMapper: (input) {
-          switch (input) {
-            case "Yes":
-              return task.steps[0].stepIdentifier;
-            case "No":
-              return task.steps[7].stepIdentifier;
-            default:
-              return null;
-          }
-        },
-      ),
-    );
     return Future.value(task);
-  }
-
-  Future<Task> getJsonTask() async {
-    final taskJson =
-        await rootBundle.loadString('survey_assets/example_json.json');
-    final taskMap = json.decode(taskJson);
-
-    return Task.fromJson(taskMap);
   }
 }
