e631d58d0d7807e83f593b4e0cd4bd58d4710085
J Harley <julian.harley@gmail.com>
2023-06-30 15:02:09 -0300

    remove all the nice syncing of data; show splash as transition (1 sec); update logo

--- consent_app/consent_app/lib/src/intro/intro.dart
+++ consent_app/consent_app/lib/src/intro/intro.dart
@@ -1,36 +1,8 @@
 import 'dart:async';
-import 'dart:convert';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:consent_app/src/components/frame.dart';
-import 'package:consent_app/src/intro/unsyncedCountWidget.dart';
 import 'package:consent_app/src/procedure_chooser_feature/procedure_item_list_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
-
-import '../../database.dart';
 import '../../main.dart';
-import 'dart:developer' as developer;
-
-Future<void> syncData() async {
-  developer.log('sync database');
-  Store store = locator<Store>();
-  String deviceId = store.deviceId;
-  var rows = await getUnsyncedFeedback();
-  List datarow = rows
-      .map((e) => {
-            ...e,
-            ...{'deviceId': deviceId}
-          })
-      .toList();
-  String jsonData = jsonEncode(datarow);
-  Future<bool> responseSuccess = postData(jsonData);
-  var ids = rows.map((ele) => ele['id']).toList();
-  if (await responseSuccess) {
-    await updateAllSurveyData(ids);
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -41,154 +13,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  ConnectivityResult _connectionStatus = ConnectivityResult.none;
-  final Connectivity _connectivity = Connectivity();
-  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
-
   @override
-  void initState() {
+  initState() {
     super.initState();
-    initConnectivity();
-
-    _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-  }
-
-  @override
-  void dispose() {
-    _connectivitySubscription.cancel();
-    super.dispose();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initConnectivity() async {
-    late ConnectivityResult connectivityStatus;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      connectivityStatus = await _connectivity.checkConnectivity();
-    } on PlatformException catch (e) {
-      developer.log('Couldnt check connectivity status');
-      developer.log(e.toString());
-      return;
-    }
-    developer.log('connectivityStatus: $connectivityStatus');
-    if (connectivityStatus != ConnectivityResult.none) {
-      developer.log('attempting to sync');
-      await syncData();
-    }
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) {
-      return Future.value(null);
-    }
-
-    return _updateConnectionStatus(connectivityStatus);
-  }
-
-  Future<void> _updateConnectionStatus(
-      ConnectivityResult connectivityStatus) async {
-    setState(() {
-      _connectionStatus = connectivityStatus;
-    });
+    Timer(const Duration(seconds: 1), onClose);
   }
 
   @override
   Widget build(BuildContext context) {
     Store store = locator<Store>();
     return FrameView(
-        heading: 'Welcome',
+        heading: '',
         showSettings: true,
-        body: Center(
-            child: FittedBox(
-          fit: BoxFit.fill,
-          child: Column(
-            children: [
-              const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Text(
-                    "Consentease v 0.1.2",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 64),
-                  ),
-                ),
-              ),
-              _connectionStatus == ConnectivityResult.none
-                  ? Container()
-                  : const UnsyncedCountWidget(),
-              Text('Device ID: ${store.deviceId}'),
-              Image.asset('assets/images/medical-abstract.png'),
-              Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: IntrinsicWidth(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(30),
-                            child: ElevatedButton(
-                              child: const Text(
-                                'Continue',
-                                style: TextStyle(fontSize: 64),
-                              ),
-                              onPressed: () {
-                                syncDataWrapper();
-                                Navigator.pushReplacementNamed(
-                                  context,
-                                  ProcedureListView.routeName,
-                                );
-                              },
-                            ),
-                          ),
+        body: Container(
+          decoration: const BoxDecoration(color: Color(0xff325FF9)),
+          child: Center(
+              child: FittedBox(
+                fit: BoxFit.fill,
+                child: Column(
+                  children: [
+                    const FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Text(
+                          "Consentease v 0.1.3",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 64),
                         ),
-                      ],
+                      ),
                     ),
-                  ))
-            ],
-          ),
-        )));
+                    Text('Device ID: ${store.deviceId}'),
+                    Image.asset('assets/images/medical-abstract.png'),
+                    Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: IntrinsicWidth(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(30),
+                                  child: ElevatedButton(
+                                    child: const Text(
+                                      'Continue',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.pushReplacementNamed(
+                                        context,
+                                        ProcedureListView.routeName,
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ))
+                  ],
+                ),
+              )),
+        ));
   }
 
-  Future<void> syncDataWrapper() async {
-    await connectivityCheck(_connectionStatus);
+  void onClose() {
+    Navigator.pushReplacementNamed(
+      context,
+      ProcedureListView.routeName);
   }
 
-  Future<void> connectivityCheck(ConnectivityResult connectionStatus) async {
-    if (connectionStatus == ConnectivityResult.none) {
-      developer.log('No connection');
-      return;
-    }
-  }
 }
 
-Future<bool> postData(String jsonString) async {
-  try {
-    if (jsonString == '[]') {
-      return false;
-    }
-    final response = await http.post(
-      Uri.parse('https://medicalease-data-collect.herokuapp.com/api/feedback'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'data': jsonString,
-      }),
-    );
-
-    developer.log(response.statusCode.toString());
-    if (response.statusCode < 300) {
-      return true;
-    } else {
-      developer.log('Failed to sync data.');
-      return false;
-    }
-  } catch (err) {
-    developer.log('Caught error: $err');
-    return false;
-  }
-}
